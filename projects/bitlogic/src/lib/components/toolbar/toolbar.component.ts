3bbc4931507544d665ff31bdd833dd6e533cc0d1
Usubiaga, Juan <juan.usubiaga@proveedor.ues21.edu.ar>
2021-01-18 22:14:03 +0000

    feat: working on toolbar and button

--- projects/bitlogic/src/lib/components/toolbar/toolbar.component.ts
+++ projects/bitlogic/src/lib/components/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'bit-toolbar',
@@ -13,9 +13,19 @@ export class ToolbarComponent implements OnInit {
   @Input()
   title: string = 'Title';
 
+  @Input()
+  currentDate: string;
+
+  @Output()
+  openProfile = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onOpenProfile() {
+    this.openProfile.emit(); // TODO send user data.
   }
 
 }
