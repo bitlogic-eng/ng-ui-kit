2048d70062a3ed4eafc200e351f9cbc235fbb6f3
Bernales, Camila <camila.bernales@proveedor.ues21.edu.ar>
2021-06-30 14:09:59 +0000

    feat: Adding slider

--- projects/bitlogic/src/lib/components/slider/slider.module.ts
+++ projects/bitlogic/src/lib/components/slider/slider.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SliderComponent } from './slider.component';
-
+import { NguCarouselModule } from '@ngu/carousel';
 
 
 @NgModule({
   declarations: [SliderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NguCarouselModule
   ],
   exports: [SliderComponent]
 })
