c9d687275b2b52843a50e3d5695831753fc597fe
Juan Usubiaga <jusubiaga@gmail.com>
2021-08-28 10:02:31 -0300

    fix: menu

--- projects/bitlogic/src/lib/components/menu/menu.component.ts
+++ projects/bitlogic/src/lib/components/menu/menu.component.ts
@@ -1,15 +1,15 @@
 import { Component, Output, EventEmitter, Input } from '@angular/core';
 
 /**
- * The Menu Opcion
- * @interface MenuOpcion
+ * The Menu Options
+ * @interface MenuOption
  */
-export interface MenuOpcion {
+export interface MenuOption {
   id: string, 
   name: string, 
   icon: string, 
-  link: string, 
-  selected: boolean
+  link: string,
+  linkType?: string
 }
 
 @Component({
@@ -20,29 +20,112 @@ export interface MenuOpcion {
 export class MenuComponent {
 
   /**
-   * Is the menu opcion 
+   * Is the menu items
+   * @type Array<MenuOption>
+   * @private
+   */
+  private _options: Array<MenuOption> = [];
+
+  /**
+   * Is the menu items
+   * @requires
    */
   @Input()
-  options: MenuOpcion[] = [];
+  set options(options: MenuOption[]) {
+    
+    this._options = options; 
+    const regexp = new RegExp("^(http|https)://", "i");
+
+    this._options.map((op) => {
+      
+      if (regexp.test(op.link)) {
+        op.linkType = "externar";
+      } else {
+        op.linkType = "internal";
+      }
+
+    })
+  }
+  get options() {
+    return this.getOptions();
+  }
+
+  /**
+   * The item showed 
+   * @default
+   */
+  @Input()
+  numberItemShowed: number = 0;
+
+  /**
+   * Is the selected option id
+   * @type string 
+   */
+  @Input()
+  selectedOptionId: string = null;
+
+  /**
+   * More Option. When the menu has several items, it's replaced for the item moreOption.
+   * @type MenuOption
+   */
+  @Input()
+  moreOption: MenuOption = null;
 
   /**
    * Menu click handler
    */
   @Output() 
-  menuClick: EventEmitter<MenuOpcion> = new EventEmitter<MenuOpcion>();
+  menuClick: EventEmitter<MenuOption> = new EventEmitter<MenuOption>();
 
+  /**
+   * Constructor
+   */
   constructor() {}
 
-  onOptionClick(optionSelected: MenuOpcion) {
-    for (let option of this.options) {
-      option.selected = false;
-      if (option.id === optionSelected.id) {
-        option.selected = true;
-      }
+  /**
+   * Check the item selected
+   * @returns boolean
+   */
+  isOptionSelected(option: MenuOption) : boolean {
+    return this.selectedOptionId === option.id;
+  }
+  
+  /**
+   * Return the menu items
+   * @returns Array<MenuOption>
+   */
+  // getOptions(): MenuOption[] {
+  //   let optionsToShow: MenuOption[]  = this._options.slice();
+
+  //   if (this.numberItemShowed !== 0 && this.moreOption !== null)
+  //   { 
+  //     const length = optionsToShow.length - this.numberItemShowed;
+  //     optionsToShow = optionsToShow.slice(0,length < 0 ? 0 : length).concat([this.moreOption]);
+  //   }
+
+  //   return optionsToShow;
+  // }
+
+  getOptions(): MenuOption[] {
+    let optionsToShow: MenuOption[] = this._options.slice();
+
+    if (this.moreOption !== null && this.numberItemShowed > 0 && this.numberItemShowed < this._options.length)
+    { 
+      // const length = optionsToShow.length - this.numberItemShowed;
+      optionsToShow = optionsToShow.slice(0,this.numberItemShowed).concat([this.moreOption]);
     }
 
-    this.menuClick.emit(optionSelected);
+    return optionsToShow;
+  }
 
+
+  /**
+   * Menu click handler
+   * @param MenuOption
+   */
+  onOptionClick(optionSelected: MenuOption) {
+    this.selectedOptionId = optionSelected.id;
+    this.menuClick.emit(optionSelected);
   }
 
 }
