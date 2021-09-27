4a92ba2f55628e796e2aae8ae162814ee486eea0
Juan Usubiaga <jusubiaga@gmail.com>
2021-09-27 20:10:57 -0300

    fix: select multiselect

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
+
 setCompodocJson(docJson);
 
 export const parameters = {
--- projects/bitlogic/src/lib/components/button/button.stories.ts
+++ projects/bitlogic/src/lib/components/button/button.stories.ts
@@ -1,34 +1,42 @@
 import { Story, Meta, moduleMetadata} from '@storybook/angular';
 import { ButtonComponent } from './button.component';
 import { ButtonModule } from './button.module';
+import { MatButtonModule } from '@angular/material/button';
 
 export default {
   title: 'Components/Button',
   component: ButtonComponent,
-  argTypes: {},
+  // argTypes: {},
+  decorators: [
+    moduleMetadata({
+      declarations: [],
+      imports: [
+        MatButtonModule,
+      ],
+    }),
+  ],
+
 } as Meta;
 
 const Template: Story<ButtonComponent> = (args: ButtonComponent) => ({
   component: ButtonComponent,
   props: args,
-  decorators: [
-    moduleMetadata({
-      declarations: [],
-      imports: [
-        ButtonModule
-      ],
-    }),
-  ],
 
-  template: `
-  <bit-button sty="text" color="secondary">Text - secondary</bit-button>
+  template: 
+//   `<button mat-raised-button color="primary" >
+//   hola
+// </button>`
+
+  `<bit-button sty="text" color="secondary">Text - secondary</bit-button>
   <bit-button sty="raised" color="secondary">raised - secondary</bit-button>
   <bit-button sty="outlined" color="secondary">outlined - secondary</bit-button>
   <bit-button sty="text" type="fab" color="secondary"><i class="material-icons mx-auto">menu_book</i></bit-button>
   <bit-button sty="raised" type="fab" color="secondary"><i class="material-icons mx-auto">menu_book</i>
   </bit-button>
   <bit-button sty="outlined" type="fab" color="secondary"><i class="material-icons mx-auto">menu_book</i>
-  </bit-button>`,
+  </bit-button>`
+
+
 });
 
 export const Default = Template.bind({});
